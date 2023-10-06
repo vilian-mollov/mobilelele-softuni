52c5a965e7f8fa46b10ac990cb546795c5a41fd2
Avalon <vilian3737@gmail.com>
2023-10-06 12:39:10 +0300

    Git initial commit for mobilelele Spring project for course Spring-Fundamentals

---
+++ src/main/java/com/softuni/mobilelelesoftuni/MobileleleSoftuniApplication.java
@@ -0,0 +1,13 @@
+package com.softuni.mobilelelesoftuni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileleleSoftuniApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MobileleleSoftuniApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/softuni/mobilelelesoftuni/MobileleleSoftuniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softuni.mobilelelesoftuni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileleleSoftuniApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
