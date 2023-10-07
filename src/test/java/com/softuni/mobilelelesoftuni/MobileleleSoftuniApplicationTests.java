9a51e324a1e2419c0f6b20707b4d50d7df342c58
Avalon <vilian3737@gmail.com>
2023-10-07 10:53:27 +0300

    Changed to Maven

--- src/test/java/com/softuni/mobilelelesoftuni/MobileleleSoftuniApplicationTests.java
+++ src/test/java/com/softuni/mobilelelesoftuni/MobileleleSoftuniApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MobileleleSoftuniApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
