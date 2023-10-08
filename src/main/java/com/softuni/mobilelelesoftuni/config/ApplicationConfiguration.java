eb82fc2066e307758b8d670df7c3da51005bef2a
Avalon <vilian3737@gmail.com>
2023-10-08 15:31:51 +0300

    imported ModelMapper and created bean for use

--- src/main/java/com/softuni/mobilelelesoftuni/config/ApplicationConfiguration.java
+++ src/main/java/com/softuni/mobilelelesoftuni/config/ApplicationConfiguration.java
@@ -1,4 +1,27 @@
 package com.softuni.mobilelelesoftuni.config;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class ApplicationConfiguration {
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+
+//    @Bean
+//    public Gson gson(){
+//        return new GsonBuilder().setPrettyPrinting().create();
+//    }
+//
+//    @Bean
+//    public PasswordEncoder passwordEncoder(){
+//        return Pbkdf2PasswordEncoder.defaultsForSpringSecurity_v5_8();
+//    }
+
+
+
 }
