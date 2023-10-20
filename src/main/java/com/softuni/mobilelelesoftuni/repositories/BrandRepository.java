8362e345361f3255ac895f48b0325673cd28ef44
Avalon <vilian3737@gmail.com>
2023-10-21 01:08:44 +0300

    Implemented Thymeleaf validations and getAllBrands method in the BrandRepository

--- src/main/java/com/softuni/mobilelelesoftuni/repositories/BrandRepository.java
+++ src/main/java/com/softuni/mobilelelesoftuni/repositories/BrandRepository.java
@@ -1,9 +1,21 @@
 package com.softuni.mobilelelesoftuni.repositories;
 
 import com.softuni.mobilelelesoftuni.models.entities.Brand;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.*;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface BrandRepository extends JpaRepository<Brand, Long> {
+
+    //  @Query("SELECT b FROM BrandEntity b "
+//      + "JOIN FETCH b.models ")
+    @EntityGraph(
+            value = "brandWithModels",
+            attributePaths = "models"
+    )
+    @Query("SELECT b FROM Brand b")
+    List<Brand> getAllBrands();
+
 }
--- src/main/java/com/softuni/mobilelelesoftuni/web/OfferController.java
+++ src/main/java/com/softuni/mobilelelesoftuni/web/OfferController.java
@@ -1,24 +1,61 @@
 package com.softuni.mobilelelesoftuni.web;
 
+import com.softuni.mobilelelesoftuni.models.dtos.CreateOfferDTO;
+import com.softuni.mobilelelesoftuni.models.entities.enums.Engine;
+import com.softuni.mobilelelesoftuni.services.BrandService;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/offers")
 public class OfferController {
 
+
+    private final BrandService brandService;
+
+    public OfferController(BrandService brandService) {
+        this.brandService = brandService;
+    }
+
+
+    @ModelAttribute("engines")
+    public Engine[] engines() {
+        return Engine.values();
+    }
+
     @GetMapping("/add")
-    public ModelAndView getAddOffer(ModelAndView modelAndView) {
-        modelAndView.setViewName("offer-add");
-        return modelAndView;
+    public String add(Model model) {
+
+        if (!model.containsAttribute("createOfferDTO")) {
+            model.addAttribute("createOfferDTO", new CreateOfferDTO());
+        }
+
+        model.addAttribute("brands", brandService.getAllBrands() );
+
+        return "offer-add";
     }
 
     @PostMapping("/add")
-    public ModelAndView addOffer(ModelAndView modelAndView) {
-        modelAndView.setViewName("offer-add");
-        //todo add offer
-        return modelAndView;
+    public String add(
+            @Valid CreateOfferDTO createOfferDTO,
+            BindingResult bindingResult,
+            RedirectAttributes rAtt) {
+
+        if(bindingResult.hasErrors()){
+            rAtt.addFlashAttribute("createOfferDTO", createOfferDTO);
+            rAtt.addFlashAttribute("org.springframework.validation.BindingResult.createOfferDTO", bindingResult);
+            return "redirect:/offers/add";
+        }
+
+
+//        UUID newOfferUUID = offerService.createOffer(createOfferDTO);
+
+        return "redirect:/offer/";
     }
 
     @GetMapping("/all")
