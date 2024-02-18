12bef8ddea71b1d5d61384108fd48bd0ffcd94d1
Avalon <vilian3737@gmail.com>
2024-02-18 10:54:36 +0200

    error page and refactoring

--- src/main/java/com/softuni/mobilelelesoftuni/services/UserService.java
+++ src/main/java/com/softuni/mobilelelesoftuni/services/UserService.java
@@ -1,7 +1,6 @@
 package com.softuni.mobilelelesoftuni.services;
 
 import com.softuni.mobilelelesoftuni.models.dtos.CreateUserDTO;
-import com.softuni.mobilelelesoftuni.models.dtos.LoginUserDTO;
 
 public interface UserService {
 
